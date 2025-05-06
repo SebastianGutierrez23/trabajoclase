5ed3a18ca358ab7de89fbea2416e1735c7090ab8
SebastianGutierrez23 <148462501+SebastianGutierrez23@users.noreply.github.com>
2025-05-05 21:18:19 -0500

    actual

---
+++ main.py
@@ -0,0 +1,17 @@
+# main.py
+
+from inventario import mostrar_inventario, agregar_producto, actualizar_producto, eliminar_producto
+
+# Diccionario inicial de inventario
+inventario = {
+    'manzanas': 60,
+    'naranjas': 30,
+    'peras': 20
+}
+
+# Uso del sistema de inventario
+mostrar_inventario(inventario)
+actualizar_producto(inventario, 'peras', 25)
+agregar_producto(inventario, 'bananas', 40)
+eliminar_producto(inventario, 'manzanas')
+mostrar_inventario(inventario)
